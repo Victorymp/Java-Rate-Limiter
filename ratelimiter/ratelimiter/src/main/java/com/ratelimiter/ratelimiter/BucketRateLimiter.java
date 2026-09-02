b0fe29c1d1569344503f8aea57747a629d0697f2
Victory Mpokosa <mpokosa5@gmail.com>
2026-09-02 23:09:00 +0100

    Implemented overflow and fixed refilling

--- ratelimiter/ratelimiter/src/main/java/com/ratelimiter/ratelimiter/BucketRateLimiter.java
+++ ratelimiter/ratelimiter/src/main/java/com/ratelimiter/ratelimiter/BucketRateLimiter.java
@@ -1,12 +1,7 @@
 package com.ratelimiter.ratelimiter;
 
-import java.util.HashSet;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
-import java.util.concurrent.ConcurrentHashMap;
 
 //Token Bucket approach
 //Each user has a bucket
@@ -14,40 +9,58 @@ public class BucketRateLimiter {
 	
 	private int bucketSize;
 	private int refillRate;
-	private final Queue<ClientRequest> tokens = new PriorityQueue();
+	private int currentBucketSize;
+	private final Queue<ClientRequest> tokens = new PriorityQueue();// this would be cache
 	
 	private int overflowSize;
-	private final Queue<ClientRequest> overflowBucket = new PriorityQueue();
 	
 	
 	public BucketRateLimiter(int bucketSize, int refillRate) {
 		this.bucketSize = bucketSize;
 		this.refillRate = refillRate;
-		this.overflowSize = 3;
+		this.currentBucketSize = bucketSize;
+		this.overflowSize = 0;
 	}
 	
 	public BucketRateLimiter(int bucketSize, int refillRate, int overflowSize) {
 		this.bucketSize = bucketSize;
 		this.refillRate = refillRate;
+		this.currentBucketSize = bucketSize;
 		this.overflowSize = overflowSize;
 	}
 	
 	public boolean allowRequest() {
 		// This algorithm doesn't care who made the request
 		// Puts all request into a bucket
+		// You wouldn't run this per request 
 		long now = System.currentTimeMillis();
-		if (bucketSize > 0 ) {
-			ClientRequest lastToken = tokens.remove();
-			
-			
-			if (now - lastToken.windowStart > 1000) {
-				bucketSize = bucketSize + refillRate;
+		ClientRequest lastToken = tokens.remove();
+		
+		if (now - lastToken.windowStart > 1000) {
+			for (int i = 0; i < refillRate; i++) {
+				if (currentBucketSize < bucketSize) {					
+					currentBucketSize ++; 
+				}
+				else if (currentBucketSize == bucketSize) {
+					overflowSize ++;
+				}
 			}
-			
-			tokens.add(new ClientRequest(now,1));
-			bucketSize--;
+		}
+		
+		tokens.add(new ClientRequest(now,1));
+		
+		// Use available overflow first
+		if (overflowSize > 0) {
+			overflowSize --;
 			return true;
 		}
+		
+		if (currentBucketSize > 0 ) {
+			currentBucketSize--;
+			return true;
+		}
+		
+		
 		return false;
 		
 	}
